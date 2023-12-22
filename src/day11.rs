6c5ee686b5c2da7aedf3947a4067d9ce786b8314
pvanliefland <pierre.vanliefland@gmail.com>
2023-12-22 12:18:27 +0100

    Day 11 cleanup

--- src/day11.rs
+++ src/day11.rs
@@ -1,56 +1,53 @@
 pub fn run(input: String) {
-    let mut coords = parse_input(&input);
-    expand_space(&mut coords);
-    let distances = distances(&coords);
+    let coords = parse_input(&input);
+    println!("Part 1: {}", sum_of_distances(&coords, 2));
 
-    println!(
-        "Part 1: {}",
-        distances.iter().map(|(_, _, d)| d).sum::<usize>()
-    );
-    println!("Part 2: {}", 42);
+    println!("Part 1: {}", sum_of_distances(&coords, 1000000));
 }
 
-fn distances(coords: &Vec<(usize, usize)>) -> Vec<((usize, usize), (usize, usize), usize)> {
+fn sum_of_distances(coords: &[Position], expand_factor: usize) -> usize {
+    let mut expanded_coords = coords.to_vec();
+    let max_x = expanded_coords.iter().map(|c| c.0).max().expect("🙄");
+    let max_y = expanded_coords.iter().map(|c| c.1).max().expect("🙄");
+
+    let empty_cols = (0..=max_x)
+        .filter(|&x| !expanded_coords.iter().any(|c| c.0 == x))
+        .collect::<Vec<_>>();
+    let empty_rows = (0..=max_y)
+        .filter(|&y| !expanded_coords.iter().any(|c| c.1 == y))
+        .collect::<Vec<_>>();
+
+    for (i, x) in empty_cols.iter().enumerate() {
+        expanded_coords
+            .iter_mut()
+            .filter(|c| c.0 > x + i * (expand_factor - 1))
+            .for_each(|c| {
+                c.0 += expand_factor - 1;
+            });
+    }
+    for (i, y) in empty_rows.iter().enumerate() {
+        expanded_coords
+            .iter_mut()
+            .filter(|c| c.1 > y + i * (expand_factor - 1))
+            .for_each(|c| {
+                c.1 += expand_factor - 1;
+            });
+    }
+
     let mut pairs = vec![];
-    for i in 0..coords.len() {
-        let c1 = vec![coords[i]; coords.len()];
-        let c2s = coords[(i + 1)..coords.len()].to_vec();
+    for i in 0..expanded_coords.len() {
+        let c1 = vec![expanded_coords[i]; expanded_coords.len()];
+        let c2s = expanded_coords[(i + 1)..expanded_coords.len()].to_vec();
         let mut zipped = c1.iter().copied().zip(c2s).collect::<Vec<_>>();
         pairs.append(&mut zipped);
     }
     pairs
         .into_iter()
-        .map(|(c1, c2)| {
-            let distance = c1.0.abs_diff(c2.0) + c1.1.abs_diff(c2.1);
-            (c1, c2, distance)
-        })
-        .collect::<Vec<_>>()
+        .map(|(c1, c2)| c1.0.abs_diff(c2.0) + c1.1.abs_diff(c2.1))
+        .sum()
 }
 
-fn expand_space(coords: &mut [(usize, usize)]) {
-    let max_x = coords.iter().map(|c| c.0).max().expect("🙄");
-    let max_y = coords.iter().map(|c| c.1).max().expect("🙄");
-
-    let empty_cols = (0..=max_x)
-        .filter(|&x| !coords.iter().any(|c| c.0 == x))
-        .collect::<Vec<_>>();
-    let empty_rows = (0..=max_y)
-        .filter(|&y| !coords.iter().any(|c| c.1 == y))
-        .collect::<Vec<_>>();
-
-    for (i, x) in empty_cols.iter().enumerate() {
-        coords.iter_mut().filter(|c| c.0 > x + i).for_each(|c| {
-            c.0 += 1;
-        });
-    }
-    for (i, y) in empty_rows.iter().enumerate() {
-        coords.iter_mut().filter(|c| c.1 > y + i).for_each(|c| {
-            c.1 += 1;
-        });
-    }
-}
-
-fn parse_input(input: &str) -> Vec<(usize, usize)> {
+fn parse_input(input: &str) -> Vec<Position> {
     input
         .lines()
         .enumerate()
@@ -64,6 +61,8 @@ fn parse_input(input: &str) -> Vec<(usize, usize)> {
         .collect()
 }
 
+type Position = (usize, usize);
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -71,12 +70,22 @@ mod tests {
 
     #[test]
     fn test_day11_part1() {
-        let mut coords = parse_input(&read_input("test/day11"));
-        expand_space(&mut coords);
-        let distances = distances(&coords);
-        assert_eq!(distances.iter().map(|(_, _, d)| d).sum::<usize>(), 374);
+        assert_eq!(
+            sum_of_distances(&parse_input(&read_input("test/day11")), 2),
+            374
+        );
     }
 
     #[test]
-    fn test_day11_part2() {}
+    fn test_day11_part2() {
+        assert_eq!(
+            sum_of_distances(&parse_input(&read_input("test/day11")), 10),
+            1030
+        );
+
+        assert_eq!(
+            sum_of_distances(&parse_input(&read_input("test/day11")), 100),
+            8410
+        );
+    }
 }
